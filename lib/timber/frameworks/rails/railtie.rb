291dd40bb8c12dd79886e77ba3f3e82451f34487
Ben Johnson <bjohnson@binarylogic.com>
2016-05-31 13:06:51 -0500

    Use the app.logger

--- lib/timber/frameworks/rails/railtie.rb
+++ lib/timber/frameworks/rails/railtie.rb
@@ -7,19 +7,18 @@ module Timber
 
         initializer 'timber.bootstrap', after: :load_config_initializers do |app|
           # Grab the rails logger
-          logger = app.config.logger
+          logger = app.logger
           if logger.nil?
-            Config.logger.warn("Rails logger is nil, can't install Timber")
-            return
+            Config.logger.warn("Rails.logger is nil, can't install Timber")
+          else
+            # TODO: this overrides any custom loggers set in config. We
+            # want to honor any custom logger they set, but default to the
+            # rails logger if they dont.
+            app.config.timber.logger = logger
+
+            # Bootitup!
+            Bootstrap.bootstrap!(logger)
           end
-
-          # TODO: this overrides any custom loggers set in config. We
-          # want to honor any custom logger they set, but default to the
-          # rails logger if they dont.
-          app.config.timber.logger = logger
-
-          # Bootitup!
-          Bootstrap.bootstrap!(logger)
         end
       end
     end
