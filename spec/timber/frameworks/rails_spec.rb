a1c9cad41bf82c883ef9cc813f910e2671a2bb8f
Ben Johnson <bjohnson@binarylogic.com>
2016-07-09 21:57:18 -0500

    Add files

---
+++ spec/timber/frameworks/rails_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Timber::Frameworks::Rails do
+  describe described_class::Railtie do
+    after(:each) do
+      reset_rails_app
+    end
+
+    describe "initializer" do
+      context "with an application_key" do
+        before(:each) { Timber::Config.application_key = "key" }
+        after(:each) { Timber::Config.application_key = nil }
+
+        it "bootstraps" do
+          expect(Timber::Bootstrap).to receive(:bootstrap!).once
+          initialize_rails_app
+        end
+      end
+    end
+  end
+end
---
+++ spec/timber/probes/rack_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+describe Timber::Probes::Rack do
+  describe Timber::Probes::Rack::Middleware do
+    describe ".call" do
+      let(:app) do
+        Rack::Builder.new do
+          use Timber::Probes::Rack::Middleware
+          run lambda { |env|
+            [200, {'Content-Type' => 'text/plain'}, ['hello world']]
+          }
+        end
+      end
+      let(:context_class) { Timber::Contexts::Rack }
+
+      def request
+        Rack::MockRequest.new(app).get('/')
+      end
+
+      it "should set the context" do
+        expect(Timber::CurrentContext).to receive(:add).with(kind_of(context_class)).and_yield.once
+        request
+      end
+    end
+  end
+end
