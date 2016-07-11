11654484aecfaa5edc7a2ed100b1dad6a77ba014
Ben Johnson <bjohnson@binarylogic.com>
2016-07-11 17:24:41 -0500

    Rename variable, apparently it conflicts with rails 3.0.0

--- lib/timber/patterns/class_level_inheritable_attributes.rb
+++ lib/timber/patterns/class_level_inheritable_attributes.rb
@@ -6,18 +6,18 @@ module Timber
 
     module ClassMethods
       def inheritable_attributes(*args)
-        @inheritable_attributes ||= [:inheritable_attributes]
-        @inheritable_attributes += args
+        @inheritable_attributes_array ||= [:inheritable_attributes]
+        @inheritable_attributes_array += args
         args.each do |arg|
           class_eval %(
             class << self; attr_accessor :#{arg} end
           )
         end
-        @inheritable_attributes
+        @inheritable_attributes_array
       end
 
       def inherited(subclass)
-        (@inheritable_attributes || []).each do |inheritable_attribute|
+        (@inheritable_attributes_array || []).each do |inheritable_attribute|
           instance_var = "@#{inheritable_attribute}"
           subclass.instance_variable_set(instance_var, instance_variable_get(instance_var))
         end
