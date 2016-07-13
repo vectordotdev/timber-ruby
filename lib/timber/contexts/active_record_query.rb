f1917f3312ae281e5c31f2dd0471f004faf45c50
Ben Johnson <bjohnson@binarylogic.com>
2016-07-13 15:04:41 -0500

    Fix AR query

--- lib/timber/contexts/active_record_query.rb
+++ lib/timber/contexts/active_record_query.rb
@@ -4,15 +4,15 @@ module Timber
       VERSION = "1".freeze
       KEY_NAME = "ruby_active_record_query".freeze
 
-      property :connection_id, :statement_name, :transaction_id, :time_ms
+      property :binds, :connection_id, :sql, :statement_name, :transaction_id, :time_ms
 
       def initialize(event)
         payload = event.payload
         @binds = formatted_binds(payload[:binds])
         @connection_id = payload[:connection_id]
+        @sql = payload[:sql].try(:strip)
         @statement_name = payload[:statement_name]
         @transaction_id = event.transaction_id
-        @sql = payload[:sql].try(:strip)
         @time_ms = event.duration
         super()
       end
