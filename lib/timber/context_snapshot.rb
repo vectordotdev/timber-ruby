23054c461d5fade4c8198c7b0e02039448c1437e
Ben Johnson <bjohnson@binarylogic.com>
2016-07-13 13:07:07 -0500

    Use key_name

--- lib/timber/context_snapshot.rb
+++ lib/timber/context_snapshot.rb
@@ -13,7 +13,7 @@ module Timber
       # It leverages the context.to_json cached string. It also
       # avoids creating an uneccessary hash.
       @json = "{"
-      @json += "\"indexes\": #{indexes.to_json}, "
+      @json += "\"indexes\": #{indexes_hash.to_json}, "
       @json += "\"hierarchy\": #{hierarchy.to_json}, "
       @json += "\"data\": {"
       last_index = size - 1
@@ -31,5 +31,14 @@ module Timber
     def size
       stack.size
     end
+
+    private
+      def indexes_hash
+        @indexes_hash ||= {}.tap do |hash|
+          indexes.each do |context, index|
+            hash[context.key_name] = index
+          end
+        end
+      end
   end
 end
