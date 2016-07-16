acef4ce0d7702d401c1e5756c3a0716a7de76c49
Ben Johnson <bjohnson@binarylogic.com>
2016-07-16 16:49:17 -0500

    Check for logger=

--- spec/support/action_view.rb
+++ spec/support/action_view.rb
@@ -1,4 +1,4 @@
 require "action_view"
 
 # Needed for the ActionView::LogSubscriber. If a logger is not present, nothing will be logged.
-ActionView::Base.logger = Rails.logger
+ActionView::Base.logger = Rails.logger if ActionView::Base.respond_to?(:logger=)
