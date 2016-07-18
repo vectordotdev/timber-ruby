d0a20afc1942d6f40bbcd2b802cbfbf000cf4178
Ben Johnson <bjohnson@binarylogic.com>
2016-07-18 11:10:21 -0500

    Fix include

--- lib/timber/probes/heroku.rb
+++ lib/timber/probes/heroku.rb
@@ -1,6 +1,10 @@
 module Timber
   module Probes
     class Heroku < Probe
+      class << self
+        attr_accessor :inserted
+      end
+
       def initialize
         if dyno.nil?
           raise RequirementNotMetError.new("The DYNO environment variable is not set. " +
@@ -9,11 +13,12 @@ module Timber
       end
 
       def insert!
-        return true if CurrentContext.incude?(Contexts::Heroku)
+        return true if self.class.inserted == true
         context = Contexts::Heroku.new(dyno)
         # Note we don't use a block here, this is because
         # the context is persistent.
         CurrentContext.add(context)
+        self.class.inserted = true
       end
 
       private
