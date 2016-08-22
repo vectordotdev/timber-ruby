f9a9273f2a07021367969708530f1a2c4fe528fe
Ben Johnson <bjohnson@binarylogic.com>
2016-08-21 23:33:48 -0500

    Attempt a carriage return

--- lib/timber/log_devices/io.rb
+++ lib/timber/log_devices/io.rb
@@ -33,7 +33,7 @@ module Timber
           end
 
           def final_message
-            @final_message ||= "#{base_message} #{context_message}\n"
+            @final_message ||= "#{context_message}\r#{base_message}\n"
           end
 
           def log_line
