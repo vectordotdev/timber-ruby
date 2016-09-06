ea91e192adb5731e36f8f4392c567d574006820b
Ben Johnson <bjohnson@binarylogic.com>
2016-09-06 08:53:35 -0500

    No limit

--- lib/timber/log_line.rb
+++ lib/timber/log_line.rb
@@ -10,7 +10,7 @@ module Timber
     attr_reader :context_snapshot, :dt, :line_indexes, :message
 
     def initialize(message)
-      puts "\n\n#{message}\n#{caller[0..15].join("\n")}"
+      puts "\n\n#{message}\n#{caller.join("\n")}"
       @dt = Time.now.utc # Capture the time as soon as possible
       message = message.to_s
       if message.bytesize > APISettings::MESSAGE_BYTE_SIZE_MAX
