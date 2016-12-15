bc5541ddf89f09ca997f7a8fb2d9dd6f0df462dd
Ben Johnson <bjohnson@binarylogic.com>
2016-12-15 11:36:51 -0600

    Only if ActiveSupport::TaggedLogging is defined

--- spec/timber/logger_spec.rb
+++ spec/timber/logger_spec.rb
@@ -76,15 +76,17 @@ describe Timber::Logger, :rails_23 => true do
       end
     end
 
-    context "with TaggedLogging", :rails_23 => false do
-      let(:logger) { ActiveSupport::TaggedLogging.new(Timber::Logger.new(io)) }
+    if defined?(ActiveSupport::TaggedLogging)
+      context "with TaggedLogging", :rails_23 => false do
+        let(:logger) { ActiveSupport::TaggedLogging.new(Timber::Logger.new(io)) }
 
-      it "should format properly with events" do
-        message = Timber::Events::SQLQuery.new(sql: "select * from users", time_ms: 56, message: "select * from users")
-        logger.tagged("tag") do
-          logger.info(message)
+        it "should format properly with events" do
+          message = Timber::Events::SQLQuery.new(sql: "select * from users", time_ms: 56, message: "select * from users")
+          logger.tagged("tag") do
+            logger.info(message)
+          end
+          expect(io.string).to eq("select * from users @timber.io {\"level\":\"info\",\"dt\":\"2016-09-01T12:00:00.000000Z\",\"event\":{\"sql_query\":{\"sql\":\"select * from users\",\"time_ms\":56}},\"context\":{\"tags\":[\"tag\"]}}\n")
         end
-        expect(io.string).to eq("select * from users @timber.io {\"level\":\"info\",\"dt\":\"2016-09-01T12:00:00.000000Z\",\"event\":{\"sql_query\":{\"sql\":\"select * from users\",\"time_ms\":56}},\"context\":{\"tags\":[\"tag\"]}}\n")
       end
     end
 
