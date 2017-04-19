14ec0f0dfc1875dfe2f094d10a3420041ebb7774
Ben Johnson <bjohnson@binarylogic.com>
2017-04-19 09:41:15 -0500

    Fix controller call params sanitization

--- lib/timber/util/hash.rb
+++ lib/timber/util/hash.rb
@@ -2,6 +2,8 @@ module Timber
   module Util
     # @private
     module Hash
+      SANITIZED_VALUE = '[sanitized]'.freeze
+
       extend self
 
       def deep_compact(hash)
@@ -20,6 +22,17 @@ module Timber
         end
 
         new_hash
+      end
+
+      def sanitize(hash, keys_to_sanitize)
+        hash.each_with_object({}) do |(k, v), h|
+          k = k.to_s.downcase
+          if keys_to_sanitize.include?(k)
+            h[k] = SANITIZED_VALUE
+          else
+            h[k] = v
+          end
+        end
       end
     end
   end
