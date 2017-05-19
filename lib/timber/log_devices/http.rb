0646e324f5107053a426ca22839add84022007e6
Ben Johnson <bjohnson@binarylogic.com>
2017-05-19 10:51:31 -0500

    Put back URLs

--- lib/timber/log_devices/http.rb
+++ lib/timber/log_devices/http.rb
@@ -61,7 +61,7 @@ module Timber
         end
       end
 
-      TIMBER_URL = "https://logs-staging.timber.io/frames".freeze
+      TIMBER_URL = "https://logs.timber.io/frames".freeze
       CONTENT_TYPE = "application/msgpack".freeze
       USER_AGENT = "Timber Ruby/#{Timber::VERSION} (HTTP)".freeze
 
