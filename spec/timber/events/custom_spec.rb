f0b46d29c1254a63d93966deaf9325484df3cd85
Ben Johnson <bjohnson@binarylogic.com>
2017-06-01 01:14:25 -0500

    Fix specs

--- spec/timber/events/custom_spec.rb
+++ spec/timber/events/custom_spec.rb
@@ -10,6 +10,7 @@ describe Timber::Events::Custom, :rails_23 => true do
 
     it "should coerce a Time into a float representing fractional milliseconds" do
       timer = Timber::Timer.start
+      sleep(0.25)
       custom_event = described_class.new(:type => :my_event, :message => "hello", :data => {:time_ms => timer})
       expect(custom_event.message).to include("in ")
       expect(custom_event.message).to end_with("ms")
