6f7d2cdfd294aab773ef67e4b81edca7bdc6610e
Ben Johnson <bjohnson@binarylogic.com>
2017-06-07 10:40:48 -0500

    Remove debug line

--- lib/timber/logger.rb
+++ lib/timber/logger.rb
@@ -224,7 +224,6 @@ module Timber
     # @note The formatter cannot be changed if you are using the HTTP logger backend.
     def formatter=(value)
       if @initialized && @logdev && @logdev.dev.is_a?(Timber::LogDevices::HTTP) && !value.is_a?(PassThroughFormatter)
-        raise @logdev.inspect
         raise ArgumentError.new("The formatter cannot be changed when using the " +
           "Timber::LogDevices::HTTP log device. The PassThroughFormatter must be used for proper " +
           "delivery.")
