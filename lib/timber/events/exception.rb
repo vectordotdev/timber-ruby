527763338dd2692ee955a77a5df372adaf721ad9
Ben Johnson <bjohnson@binarylogic.com>
2017-08-04 01:07:13 -0500

    Add deprecated Exception class

---
+++ lib/timber/events/exception.rb
@@ -0,0 +1,9 @@
+require "timber/events/error"
+
+module Timber
+  module Events
+    # DEPRECATION: This class is deprecated in favor of using {Timber:Events:Error}.
+    class Exception < Error
+    end
+  end
+end
