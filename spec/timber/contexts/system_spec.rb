f8b40f82d08380ab4727d2a50522bcc1555acbf5
Ben Johnson <bjohnson@binarylogic.com>
2017-08-29 15:59:48 -0500

    Coerce vm_pid into a String

--- spec/timber/contexts/system_spec.rb
+++ spec/timber/contexts/system_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Timber::Contexts::System, :rails_23 => true do
   describe ".as_json" do
-    it "should coerce pid into an string" do
+    it "should coerce pid into an integer" do
       custom_context = described_class.new(:pid => "1")
       json = custom_context.as_json()
       expect(json[:pid]).to eq(1)
